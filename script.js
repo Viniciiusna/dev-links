7895f2fb9c16569d5720f9ac5e6fab97f3d627a9
Viniciusna <sefaz.vinicius22@gmail.com>
2025-03-16 20:18:37 -0300

    ajeitando erros

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function togglemode() {
   const html = document.documentElement
-  button.addEventListener("click", togglemode)
 
-  //const html = document.documentElement
-
-  //if (html.classList.contains('light')) {
-  //  html.classList.remove('light')
-  //} else {
-  //    html.classList.add('light')
-
+  //  if (html.classList.contains("light")) {
+  //  html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
   html.classList.toggle("light")
 }
